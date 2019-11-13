db9f7f801aeea1f9028b34e2d064ef0461c049e4
aaron <aaron@192.168.0.59>
2019-11-13 19:34:02 +0000

    add pet test from owner

--- src/oop19_ca2_aaron_reihill/Main.java
+++ src/oop19_ca2_aaron_reihill/Main.java
@@ -20,15 +20,15 @@ public class Main
         Owner bob = new Owner("bob");
         Owner tim = new Owner("tim");
         Owner sam = new Owner("sam");
-        
-        Pet p2 = new Pet(sam.getId(),"Cat", "sarah","Poddle", 4, "Blue", "female", "20-20-10");
-        Pet p3 = new Pet(bob.getId(),"Fish", "Poddle", 1, "Black", "21-20-10");
-        Pet p4 = new Pet(bob.getId(),"GoldFish", "Poddle", 6, "Yellow", "20-20-10");
-        Pet p1 = new Pet(sam.getId(),"Dog","kevin", "Poddle", 2, "Black", "male","4-22-10");
 
-        Pet p5 = new Pet(tim.getId(),"Pig",  "christ","Belly", 92, "Black","UNKOWN", "4-22-10");
-        Pet p6 = new Pet(sam.getId(),"Shark", "Great White", 33, "Green", "2-20-6");
-        Pet p7 = new Pet(tim.getId(),"Elephant", "Big", 100, "Yelloq", "21-11-32");
+        Pet p2 = new Pet(sam.getId(), "Cat", "sarah", "Poddle", 4, "Blue", "female", "20-20-10");
+        Pet p3 = new Pet(bob.getId(), "Fish", "Poddle", 1, "Black", "21-20-10");
+        Pet p4 = new Pet(bob.getId(), "GoldFish", "Poddle", 6, "Yellow", "20-20-10");
+        Pet p1 = new Pet(sam.getId(), "Dog", "kevin", "Poddle", 2, "Black", "male", "4-22-10");
+
+        Pet p5 = new Pet(tim.getId(), "Pig", "christ", "Belly", 92, "Black", "UNKOWN", "4-22-10");
+        Pet p6 = new Pet(sam.getId(), "Shark", "Great White", 33, "Green", "2-20-6");
+        Pet p7 = new Pet(tim.getId(), "Elephant", "Big", 100, "Yelloq", "21-11-32");
 
         List<Pet> pets = new ArrayList<>();
         pets.add(p1);
@@ -36,7 +36,7 @@ public class Main
         pets.add(p3);
         pets.add(p4);
         bob.setPets(pets);
-        
+
         List<Pet> pets2 = new ArrayList<>();
         pets.add(p5);
         pets.add(p6);
@@ -44,11 +44,14 @@ public class Main
         pets.add(p1);
         tim.setPets(pets2);
         
+        sam.addPet("Tank", "Tim", "German", 22, "Camo", "female", "4-22-10");
+        
         displayList(bob);
         System.out.println("");
         displayList(tim);
 
     }
+
     public static void displayList(Owner o)
     {
         o.displayAllPets();
