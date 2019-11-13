351324e87581934186494ae7218e6b5165be90df
aaron <aaron@192.168.0.59>
2019-11-13 17:48:14 +0000

    water enum

---
+++ src/oop19_ca2_aaron_reihill/Water.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop19_ca2_aaron_reihill;
+
+/**
+ *
+ * @author aaron
+ */
+public enum Water
+{
+    FRESHWATER, SEAWATER,BRACKISH
+}
