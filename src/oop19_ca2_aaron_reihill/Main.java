91a6b6e5d7d10cf7fbe5be8f222449c052ad3d6e
aaron <aaron@192.168.0.59>
2019-11-13 18:30:41 +0000

    simple arraylist test

--- src/oop19_ca2_aaron_reihill/Main.java
+++ src/oop19_ca2_aaron_reihill/Main.java
@@ -6,6 +6,7 @@
 package oop19_ca2_aaron_reihill;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -16,15 +17,37 @@ public class Main
 
     public static void main(String[] args)
     {
-        Pet p1 = new Pet("Dog", "Poddle", 2, "Black", "20-20-10");
-        Pet p2 = new Pet("Cat", "Poddle", 2, "Black", "20-20-10");
-        Pet p3 = new Pet("Fish", "Poddle", 2, "Black", "20-20-10");
-        Pet p4 = new Pet("GoldFish", "Poddle", 2, "Black", "20-20-10");
+        
+        Pet p1 = new Pet("Dog","kevin", "Poddle", 2, "Black", "male","4-22-10");
+        Pet p2 = new Pet("Cat", "sarah","Poddle", 4, "Blue", "female", "20-20-10");
+        Pet p3 = new Pet("Fish", "Poddle", 1, "Black", "21-20-10");
+        Pet p4 = new Pet("GoldFish", "Poddle", 6, "Yellow", "20-20-10");
+        
+        Pet p5 = new Pet("Pig",  "christ","Belly", 92, "Black","UNKOWN", "4-22-10");
+        Pet p6 = new Pet("Shark", "Great White", 33, "Green", "2-20-6");
+        Pet p7 = new Pet("Elephant", "Big", 100, "Yelloq", "21-11-32");
 
-        ArrayList<Pet> pets = new ArrayList<>();
+        List<Pet> pets = new ArrayList<>();
         pets.add(p1);
         pets.add(p2);
         pets.add(p3);
         pets.add(p4);
+        
+        List<Pet> pets2 = new ArrayList<>();
+        pets.add(p5);
+        pets.add(p6);
+        pets.add(p7);
+        pets.add(p1);
+        Owner bob = new Owner("bob", pets);
+        
+        displayList(pets);
+        displayList(pets2);
+    }
+    public static void displayList(List<Pet> pets)
+    {
+        for(Pet p: pets)
+        {
+            System.out.println(p);
+        }
     }
 }
