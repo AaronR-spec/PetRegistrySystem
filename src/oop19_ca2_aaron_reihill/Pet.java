a034ba021532b8aa49e70c20246dac60cc8ee247
aaron <aaron@192.168.0.59>
2019-11-13 19:13:07 +0000

    index / id working

--- src/oop19_ca2_aaron_reihill/Pet.java
+++ src/oop19_ca2_aaron_reihill/Pet.java
@@ -5,11 +5,15 @@
  */
 package oop19_ca2_aaron_reihill;
 
+import java.util.List;
+
 /**
  *
  * @author D00222467
  */
-public class Pet {
+public class Pet
+{
+
     private String type;
     private String name;
     private String breed;
@@ -17,10 +21,11 @@ public class Pet {
     private String colour;
     private Gender gender;
     private String regDate;
-    private static int petID;
-    private static int ownerID;
+    private static int index = 0;
+    private int petID = 0;
+    private int ownerID;
 
-    public Pet() 
+    public Pet()
     {
         this.type = "";
         this.name = "";
@@ -29,18 +34,20 @@ public class Pet {
         this.colour = "";
         this.gender = gender.UNKOWN;
         this.regDate = "";
+        this.petID = this.index++;
     }
 
-    public Pet(String type, String breed, int age, String colour, String regDate) {
+    public Pet(String type, String breed, int age, String colour, String regDate)
+    {
         this.type = type;
         this.breed = breed;
         this.age = age;
         this.colour = colour;
         this.regDate = regDate;
         this.gender = gender.UNKOWN;
+        this.petID = this.index++;
     }
 
-    
     public Pet(String type, String name, String breed, int age, String colour, String gender, String regDate)
     {
         this.type = type;
@@ -50,25 +57,61 @@ public class Pet {
         this.colour = colour;
         findGender(gender);
         this.regDate = regDate;
+        this.petID = this.index++;
     }
 
-    public String getType() {
+    public Pet(int owner, String type, String breed, int age, String colour, String regDate)
+    {
+        this.ownerID = owner;
+        this.type = type;
+        this.breed = breed;
+        this.age = age;
+        this.colour = colour;
+        this.regDate = regDate;
+        this.gender = gender.UNKOWN;
+        this.petID = this.index++;
+    }
+
+    public Pet(int owner, String type, String name, String breed, int age, String colour, String gender, String regDate)
+    {
+        this.ownerID = owner;
+        this.type = type;
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.colour = colour;
+        findGender(gender);
+        this.regDate = regDate;
+        this.petID = this.index++;
+    }
+
+    public String getType()
+    {
         return type;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public String getBreed() {
+    public String getBreed()
+    {
         return breed;
     }
 
-    public int getAge() {
+    public int getAge()
+    {
         return age;
     }
 
-    public String getColour() {
+    public static int getIndex()
+    {
+        return index;
+    }
+
+    public String getColour()
+    {
         return colour;
     }
 
@@ -77,35 +120,43 @@ public class Pet {
         return gender;
     }
 
-    public String getRegDate() {
+    public String getRegDate()
+    {
         return regDate;
     }
 
-    public static int getPetID() {
+    public int getPetID()
+    {
         return petID;
     }
 
-    public static int getOwnerID() {
+    public int getOwnerID()
+    {
         return ownerID;
     }
 
-    public void setType(String type) {
+    public void setType(String type)
+    {
         this.type = type;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public void setBreed(String breed) {
+    public void setBreed(String breed)
+    {
         this.breed = breed;
     }
 
-    public void setAge(int age) {
+    public void setAge(int age)
+    {
         this.age = age;
     }
 
-    public void setColour(String colour) {
+    public void setColour(String colour)
+    {
         this.colour = colour;
     }
 
@@ -114,29 +165,47 @@ public class Pet {
         this.gender = gender;
     }
 
-
-    public void setRegDate(String regDate) {
+    public void setRegDate(String regDate)
+    {
         this.regDate = regDate;
     }
 
-    public static void setPetID(int petID) {
-        Pet.petID = petID;
+    public static void setIndex(int index)
+    {
+        Pet.index = index;
     }
 
-    public static void setOwnerID(int ownerID) {
-        Pet.ownerID = ownerID;
-    }
-    public static Pet createPet(String type, String name, String breed, int age, String colour, String gender, String regDate)
+    public void setPetID(int petID)
     {
-        return new Pet(type,name,breed,age,colour,gender,regDate);
+        this.petID = petID;
     }
+
+    public void setOwnerID(int ownerID)
+    {
+        this.ownerID = ownerID;
+    }
+
+    public Pet createPet(String type, String name, String breed, int age, String colour, String gender, String regDate)
+    {
+        this.petID++;
+        return new Pet(type, name, breed, age, colour, gender, regDate);
+    }
+
+    public void displayAllPets(List<Pet> pets)
+    {
+        for (Pet p : pets)
+        {
+            System.out.println("PetID: " + p.getPetID()+ ", OwnerID: " + p.getOwnerID() + ", Type: " + p.getType() + ", Name: " + p.getName() + ", Age:" + p.getAge() + ", Breed: " + p.getBreed() + ", Colour:" + p.getColour() + ", Gender: " + p.getGender() + ", DateReg: " + p.getRegDate());
+        }
+    }
+
     private void findGender(String g)
     {
-        if(g.equalsIgnoreCase("male"))
+        if (g.equalsIgnoreCase("male"))
         {
             this.gender = this.gender.MALE;
         }
-        else if(g.equalsIgnoreCase("female"))
+        else if (g.equalsIgnoreCase("female"))
         {
             this.gender = this.gender.FEMALE;
         }
@@ -145,10 +214,11 @@ public class Pet {
             this.gender = this.gender.UNKOWN;
         }
     }
+
     @Override
-    public String toString() {
-        return getClass().getSimpleName()+ "{" + "type=" + type + ", name=" + name + ", breed=" + breed + ", age=" + age + ", colour=" + colour + ", gender=" + gender + ", regDate=" + regDate + '}';
+    public String toString()
+    {
+        return getClass().getSimpleName() + "{" + "type=" + type + ", name=" + name + ", breed=" + breed + ", age=" + age + ", colour=" + colour + ", gender=" + gender + ", regDate=" + regDate + '}';
     }
-    
-    
+
 }
