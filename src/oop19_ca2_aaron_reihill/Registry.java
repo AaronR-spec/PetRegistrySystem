3311d0eb474f4de9d2aa0265763d950c7a7c0cdc
aaron <aaron@192.168.0.59>
2019-11-14 10:17:16 +0000

    basic registry

---
+++ src/oop19_ca2_aaron_reihill/Registry.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop19_ca2_aaron_reihill;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author D00222467
+ */
+public class Registry 
+{
+    ArrayList<Owner> owners;
+
+    public ArrayList<Owner> getOwners() 
+    {
+        return owners;
+    }
+
+    public void setOwners(ArrayList<Owner> owners) 
+    {
+        this.owners = owners;
+    }
+    
+}
