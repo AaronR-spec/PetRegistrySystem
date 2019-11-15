e2dbaf79866ba3909dfa7828abb5d4a0a7e02849
aaron <aaron@192.168.0.59>
2019-11-15 19:11:04 +0000

    owner add dup check

--- src/oop19_ca2_aaron_reihill/Registry.java
+++ src/oop19_ca2_aaron_reihill/Registry.java
@@ -12,55 +12,104 @@ import java.util.List;
  *
  * @author D00222467
  */
-public class Registry 
+public class Registry
 {
-    ArrayList<Owner> owners;
 
-    public ArrayList<Owner> getOwners() 
+    ArrayList<Owner> owners = new ArrayList<>();
+
+    public Registry()
+    {
+    }
+
+    public Registry(ArrayList<Owner> owners)
+    {
+        this.owners = owners;
+    }
+
+    public ArrayList<Owner> getOwners()
     {
         return owners;
     }
 
-    public void setOwners(ArrayList<Owner> owners) 
+    public void setOwners(ArrayList<Owner> owners)
     {
         this.owners = owners;
     }
+
     public void addOwner(String name)
     {
-        owners.add(new Owner(name));
+        this.owners.add(new Owner(name));
     }
+
+    public void addOwner(Owner o)
+    {
+        if (CheckOwnerDup(o.getName(), o.getAddress()) ==  -1)
+        {
+            this.owners.add(o);
+        }
+        else
+        {
+            System.out.println("Owner Already Registered");
+        }
+    }
+
     public void addOwner(String name, String email, String telephone, String address, List<Pet> pets)
     {
-        owners.add(new Owner(name,email,telephone,address,pets));
+        if (CheckOwnerDup(name, address) ==  -1)
+        {
+            this.owners.add(new Owner(name, email, telephone, address, pets));
+        }
+        else
+        {
+            System.out.println("Owner Already Registered");
+        }
     }
+
     public void addOwner(String name, String email, String address)
     {
-        owners.add(new Owner(name,email,address));
+        if (CheckOwnerDup(name, address) ==  -1)
+        {
+            this.owners.add(new Owner(name, email, address));
+        }
+        else
+        {
+            System.out.println("Owner Already Registered");
+        }
     }
-        public void addOwner(String name, int id, String email, String telephone, String address)
+
+    public void addOwner(String name, String email, String telephone, String address)
     {
-        owners.add(new Owner(name,id,email,telephone,address));
+        if (CheckOwnerDup(name, address) ==  -1)
+        {
+            this.owners.add(new Owner(name, email, telephone, address));
+        }
+        else
+        {
+            System.out.println("Owner Already Registered");
+        }
     }
+
     public void addOwner(String name, List<Pet> pets)
     {
-        owners.add(new Owner(name,pets));
+        this.owners.add(new Owner(name, pets));
     }
+
     public void removeOwner(Owner o)
     {
-        int ownerIndex = findOwner(o.getId());
-        if(ownerIndex!= -1)
+        if (findOwner(o.getId()) == -1)
         {
-        this.owners.remove(o);
+            this.owners.remove(o);
         }
         else
         {
             System.out.println("Owner Not Found");
         }
     }
+
     public void removeOwner(int id)
     {
         int ownerIndex = findOwner(id);
-        if(ownerIndex != -1)
+        if (ownerIndex != -1)
         {
             this.owners.remove(ownerIndex);
         }
@@ -69,19 +118,46 @@ public class Registry
             System.out.println("Owner Not Found");
         }
     }
+
     private int findOwner(int id)
     {
         int index = -1;
         int i = 0;
-        boolean found =false;
-        while(!found&& i<this.owners.size())
+        boolean found = false;
+        while (!found && i < this.owners.size())
         {
-            if(this.owners.get(i).getId()== id)
+            if (this.owners.get(i).getId() == id)
             {
-               index=i;
-               found = true;
+                index = i;
+                found = true;
             }
+            i++;
         }
         return index;
     }
+
+    private int CheckOwnerDup(String name, String address)
+    {
+        int ownerIndex = -1;
+        boolean found = false;
+        int i = 0;
+        while (!found && i < this.owners.size())
+        {
+            Owner o = this.owners.get(i);
+            if (o.getName().equalsIgnoreCase(name) && o.getAddress().equalsIgnoreCase(address))
+            {
+                ownerIndex = i;
+                found = true;
+            }
+            i++;
+        }
+        return ownerIndex;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Registry{" + "owners=" + owners + '}';
+    }
+
 }
