2082bda48fa5fbf563820699e1e2878b2f715e21
aaron <aaron@192.168.0.59>
2019-11-15 19:33:57 +0000

    owner display test

--- src/oop19_ca2_aaron_reihill/Main.java
+++ src/oop19_ca2_aaron_reihill/Main.java
@@ -17,8 +17,10 @@ public class Main
 
     public static void main(String[] args)
     {
-        Owner bob = new Owner("bob");
-
+        Owner bob = new Owner("bob","etqtq","dundalk");
+        Owner kevin = new Owner("kevin");
+        Owner greg = new Owner("greg");
+        
         Pet p1 = new Mammal(bob.getId(), "Dog", "kevin", "Poddle", 2, "Black", "male", "4-22-10",true);
         Pet p2 = new Bird(bob.getId(), "Cat", "sarah", "Poddle", 4, "Blue", "female", "20-20-10",2,false);
         Pet p3 = new Fish(bob.getId(), "Fish", "Kevin","Poddle", 1, "Black","male", "21-20-10","freshwater");
@@ -27,18 +29,20 @@ public class Main
         List<Pet> pets = new ArrayList<>();
         pets.add(p1);
         pets.add(p2);
-        pets.add(p3);
         pets.add(p4);
         bob.setPets(pets);
-        
-        displayList(bob);
-        bob.addPet("Cat", "sarah", "Poddle", 4, "Blue", "female", "20-20-10",2,false);
-        displayList(bob);
-
+        List<Pet> pets2 = new ArrayList<>();
+        pets.add(p3);
+        kevin.setPets(pets2);
+        Registry r = new Registry();
+        r.addOwner(bob);
+        r.addOwner(kevin);
+        r.displayOwners();
     }
 
     public static void displayList(Owner o)
     {
+        o.displayAllPets();
         o.displayAllPets();
     }
 }
