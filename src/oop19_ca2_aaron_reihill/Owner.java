3473b112b392d1721c00f558e828b16135d1a2dd
aaron <aaron@192.168.0.59>
2019-11-25 19:33:17 +0000

    sort by age

--- src/oop19_ca2_aaron_reihill/Owner.java
+++ src/oop19_ca2_aaron_reihill/Owner.java
@@ -1,7 +1,7 @@
-
 package oop19_ca2_aaron_reihill;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -12,16 +12,16 @@ import java.util.Objects;
 public class Owner
 {
 
-    private String name ="";
+    private String name = "";
     private static int index = 0;
     private int id;
-    private String email ="";
-    private String telephone ="";
-    private String address ="";
+    private String email = "";
+    private String telephone = "";
+    private String address = "";
     private List<Pet> pets = new ArrayList<>();
     private static final String PET_ALREADY_REG = "Pet Already Registered";
     private static final String PET_NOT_FOUND = "Pet Not Found";
-    
+
     public Owner()
     {
     }
@@ -63,7 +63,8 @@ public class Owner
         this.address = address;
         this.id = index++;
     }
-    public Owner(int id,String name, String telephone, String address)
+
+    public Owner(int id, String name, String telephone, String address)
     {
         this.name = name;
         this.telephone = telephone;
@@ -74,7 +75,7 @@ public class Owner
     public Owner(String name, String email, String telephone, String address)
     {
         this.name = name;
-        
+
         this.id = Owner.index++;
         this.email = email;
         this.telephone = telephone;
@@ -174,28 +175,31 @@ public class Owner
             System.out.println(PET_ALREADY_REG);
         }
     }
-        public void addPet(int petId,String type, String name, String breed, int age, String colour, String gender)
+
+    public void addPet(int petId, String type, String name, String breed, int age, String colour, String gender)
     {
         if (CheckPetDup(type, name, breed, age, colour, gender) == -1)
         {
-            this.pets.add(Pet.createPet(this.id,petId, type, name, breed, age, colour, gender));
+            this.pets.add(Pet.createPet(this.id, petId, type, name, breed, age, colour, gender));
         }
         else
         {
             System.out.println(PET_ALREADY_REG);
         }
     }
-    public void addPet(int id,String type, String name, String breed, int age, String colour, String gender, String water)
+
+    public void addPet(int id, String type, String name, String breed, int age, String colour, String gender, String water)
     {
         if (CheckPetDup(type, name, breed, age, colour, gender) == -1)
         {
-            this.pets.add(Pet.createPet(this.id,id, type, name, breed, age, colour, gender, water));
+            this.pets.add(Pet.createPet(this.id, id, type, name, breed, age, colour, gender, water));
         }
         else
         {
             System.out.println(PET_ALREADY_REG);
         }
     }
+
     public void addPet(String type, String name, String breed, int age, String colour, String gender, String water)
     {
         if (CheckPetDup(type, name, breed, age, colour, gender) == -1)
@@ -243,11 +247,12 @@ public class Owner
             System.out.println(PET_ALREADY_REG);
         }
     }
-        public void addPet(int petId,String type, String name, String breed, int age, String colour, String gender, boolean neutered)
+
+    public void addPet(int petId, String type, String name, String breed, int age, String colour, String gender, boolean neutered)
     {
         if (CheckPetDup(type, name, breed, age, colour, gender) == -1)
         {
-            this.pets.add(Pet.createPet(this.id,petId, type, name, breed, age, colour, gender, neutered));
+            this.pets.add(Pet.createPet(this.id, petId, type, name, breed, age, colour, gender, neutered));
         }
         else
         {
@@ -278,17 +283,19 @@ public class Owner
             System.out.println(PET_ALREADY_REG);
         }
     }
-    public void addPet(int petId,String type, String name, String breed, int age, String colour, String gender, int wingspan, boolean fly)
+
+    public void addPet(int petId, String type, String name, String breed, int age, String colour, String gender, int wingspan, boolean fly)
     {
         if (CheckPetDup(type, name, breed, age, colour, gender) == -1)
         {
-            this.pets.add(Pet.createPet(this.id,petId ,type, name, breed, age, colour, gender, wingspan, fly));
+            this.pets.add(Pet.createPet(this.id, petId, type, name, breed, age, colour, gender, wingspan, fly));
         }
         else
         {
             System.out.println(PET_ALREADY_REG);
         }
     }
+
     public void removePet(Pet p)
     {
         int petIndex = findPet(p.getPetID());
@@ -314,6 +321,7 @@ public class Owner
             System.out.println(PET_NOT_FOUND);
         }
     }
+
     public void removeAllPet()
     {
         this.pets.clear();
@@ -321,12 +329,15 @@ public class Owner
 
     public void displayAllPets()
     {
-        Pet p = new Pet();
-        p.displayAllPets(this.pets);
+        Pet.displayAllPets(this.pets);
 
     }
+    public void displayAllPetsByAge()
+    {
+        PetAgeComparator age = new PetAgeComparator();
+        Pet.displayAllPets(Collections.sort(this.pets,age));
 
-   
+    }
     public int findPet(int id)
     {
         int petIndex = -1;
