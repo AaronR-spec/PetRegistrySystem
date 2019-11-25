c43ad9aef674ca1943799d65bd289a494b0545e2
aaron <aaron@192.168.0.59>
2019-11-25 19:17:02 +0000

    bird edit options

--- src/oop19_ca2_aaron_reihill/Main.java
+++ src/oop19_ca2_aaron_reihill/Main.java
@@ -869,7 +869,7 @@ public class Main
                     }
                     else if (p instanceof Bird)
                     {
-
+                        editBird(id);
                     }
                     else
                     {
@@ -1035,7 +1035,7 @@ public class Main
                     r.changePetRegDate(id, user);
                     break;
                 case 10:
-                   r.displayPetByID(id);
+                    r.displayPetByID(id);
                     System.out.print("\nChange Water Type To: ");
                     user = keyboard.next();
                     r.changePetWaterType(id, user);
@@ -1045,6 +1045,70 @@ public class Main
                     break;
             }
             if (option > 11 || option < 0)
+            {
+                System.out.println("Sorry There Is No Option " + option);
+                System.out.println("Please Choose Another Option From The List\n");
+            }
+        }
+    }
+
+    public static void editBird(int id)
+    {
+        String user;
+        int userInput;
+        boolean back = false;
+        while (!back)
+        {
+            System.out.println("\nSecond Page...");
+            System.out.println("(6) Back");
+            System.out.println("(7) Owner");
+            System.out.println("(8) Gender");
+            System.out.println("(9) Date Registered");
+            System.out.println("(10) WingSpan");
+            System.out.println("(11) Ability To Fly");
+            System.out.print("Select An Option: ");
+            int option = checkUserInputMenu(12);
+            switch (option)
+            {
+                case 6:
+                    System.out.println("Returning...");
+                    back = true;
+                    break;
+                case 7:
+                    r.displayPetByID(id);
+                    System.out.print("\nChange Owner Id To: ");
+                    userInput = checkUserInputInt("Change Owner Id To");
+                    r.changePetOwner(id, userInput);
+                    break;
+                case 8:
+                    r.displayPetByID(id);
+                    System.out.print("\nChange Gender To: ");
+                    user = checkUserInput("Change Gender To");
+                    r.changePetGender(id, user);
+                    break;
+                case 9:
+                    r.displayPetByID(id);
+                    System.out.print("\nChange Registered Date To: ");
+                    user = keyboard.next();
+                    r.changePetRegDate(id, user);
+                    break;
+                case 10:
+                    r.displayPetByID(id);
+                    System.out.print("\nChange Winspan To: ");
+                    userInput = checkUserInputInt("Change Winspan To");
+                    r.changeWingspan(id,userInput);
+                    break;
+                case 11:
+                    r.displayPetByID(id);
+                    System.out.print("\nAbility To Fly To: ");
+                    user = checkUserInput("Ability To Fly To");
+                    r.changeFly(id, user);
+                    break;
+                case 12:
+                    clearInvalidMenu();
+                    break;
+            }
+            if (option > 12 || option < 0)
             {
                 System.out.println("Sorry There Is No Option " + option);
                 System.out.println("Please Choose Another Option From The List\n");
