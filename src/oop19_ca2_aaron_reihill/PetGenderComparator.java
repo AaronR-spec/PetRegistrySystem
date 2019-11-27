6c8ac508c479fe980644056b3b87b90b7a4d3a36
aaron <aaron@192.168.0.59>
2019-11-27 18:53:52 +0000

    petGender comparator

--- src/oop19_ca2_aaron_reihill/PetGenderComparator.java
+++ src/oop19_ca2_aaron_reihill/PetGenderComparator.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package oop19_ca2_aaron_reihill;
 
 import java.util.Comparator;
@@ -13,11 +8,10 @@ import java.util.Comparator;
  */
 public class PetGenderComparator implements Comparator<Pet>
 {
-    	public int compare(Pet a, Pet b)
-	{
-		return a.getGender().compareTo(b.getGender());
 
-		// String class has a compareTo() method that
-		// returns -1, 0, or +1 as appropriate
-	}
+    @Override
+    public int compare(Pet a, Pet b)
+    {
+        return a.getGender().compareTo(b.getGender());
+    }
 }
